0a13c17a1f9b207c04f944ef0e8ee8e117048122
Tomas <tomi.alva1@gmail.com>
2022-11-01 09:56:16 -0300

    git commit -m"avances de fotos con las cards"

--- src/data/productos.js
+++ src/data/productos.js
@@ -5,7 +5,7 @@ const productos = [
         id: 'sku001',
         title: 'Bandejas e individuales',
         text:  'Lorem ipsum',
-        img: {imgcanastas},
+        img: './img/planta.jpeg',
         categorie: 'cat01',
         url: 'https://www.canva.com/design/DAE8e10OqA4/pHJ3ld7sJOOhVf-1XPE2uQ/view?utm_content=DAE8e10OqA4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#6'
     },
@@ -13,7 +13,7 @@ const productos = [
         id: 'sku002',
         title: 'Aromatizantes',
         text:  'Lorem ipsum',
-        img: 'https://picsum.photos/200/200?random=2',
+        img: './img/aromatizantes.jpg',
         categorie: 'cat02',
         url: 'https://www.canva.com/design/DAE8e10OqA4/pHJ3ld7sJOOhVf-1XPE2uQ/view?utm_content=DAE8e10OqA4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#5'
     },
@@ -21,7 +21,7 @@ const productos = [
         id: 'sku003',
         title: 'Canastas',
         text:  'Lorem ipsum',
-        img: 'https://picsum.photos/200/200?random=3',
+        img: './img/canastas.jpg',
         categorie: 'cat01',
         url: 'https://www.canva.com/design/DAE8e10OqA4/pHJ3ld7sJOOhVf-1XPE2uQ/view?utm_content=DAE8e10OqA4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#8'
     },
@@ -29,7 +29,7 @@ const productos = [
         id: 'sku004',
         title: 'Jarrones',
         text:  'Lorem ipsum',
-        img: 'https://picsum.photos/200/200?random=4',
+        img: './img/jarrones.jpg',
         categorie: 'cat02',
         url: 'https://www.canva.com/design/DAE8e10OqA4/pHJ3ld7sJOOhVf-1XPE2uQ/view?utm_content=DAE8e10OqA4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#9'
     },
@@ -37,7 +37,7 @@ const productos = [
         id: 'sku005',
         title: 'Textiles',
         text:  'Lorem ipsum',
-        img: 'https://picsum.photos/200/200?random=5',
+        img: './img/textiles.jpg',
         categorie: 'cat01',
         url: 'https://www.canva.com/design/DAE8e10OqA4/pHJ3ld7sJOOhVf-1XPE2uQ/view?utm_content=DAE8e10OqA4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#12'
     },
@@ -45,7 +45,7 @@ const productos = [
         id: 'sku006',
         title: 'Ceramicas',
         text:  'Lorem ipsum',
-        img: 'https://picsum.photos/200/200?random=5',
+        img: './img/ceramicas.jpg',
         categorie: 'cat01',
         url: 'https://www.canva.com/design/DAE8e10OqA4/pHJ3ld7sJOOhVf-1XPE2uQ/view?utm_content=DAE8e10OqA4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#11'
     },
